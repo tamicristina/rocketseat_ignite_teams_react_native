08fea28dae9d71240677e030784e1a2e690c49ba
Tamires <79336444+tamicristina@users.noreply.github.com>
2023-02-02 09:52:08 -0300

    create back button

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,13 @@
-import { Container, Logo } from "./styles";
+import { Container, Logo, BackButton, BackIcon } from "./styles";
 
 import logoImg from "@assets/logo.png";
 
 export function Header() {
   return (
     <Container>
+      <BackButton>
+        <BackIcon />
+      </BackButton>
       <Logo source={logoImg} />
     </Container>
   );
