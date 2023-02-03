93ad7107964d88d54a037461f8f71b31d90280c2
Tamires <79336444+tamicristina@users.noreply.github.com>
2023-02-03 09:37:40 -0300

    add logic to show back button

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,18 @@
 import { Container, Logo, BackButton, BackIcon } from "./styles";
-
 import logoImg from "@assets/logo.png";
 
-export function Header() {
+type Props = {
+  showBackButton?: boolean;
+};
+
+export function Header({ showBackButton = false }: Props) {
   return (
     <Container>
-      <BackButton>
-        <BackIcon />
-      </BackButton>
+      {showBackButton && (
+        <BackButton>
+          <BackIcon />
+        </BackButton>
+      )}
       <Logo source={logoImg} />
     </Container>
   );
