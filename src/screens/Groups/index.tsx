2358b85c82d4de099c37e4079b2ea10511388448
Tamires <79336444+tamicristina@users.noreply.github.com>
2023-02-08 09:52:26 -0300

    add flatlist

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,14 +1,24 @@
+import { useState } from "react";
 import { Header } from "../../components/Header";
 import { Highlight } from "../../components/Highlight";
 import { GroupCard } from "../../components/GroupCard";
 import { Container } from "./styles";
+import { FlatList } from "react-native";
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>(["Galera da Rocket"]);
   return (
     <Container>
       <Header />
       <Highlight title="Turmas" subtitle="jogue com a sua turma" />
-      <GroupCard title="Galera do Ignite" onPress={() => {}} />
+      <FlatList
+        //estado que eu quero carregar
+        data={groups}
+        //extrai um valor único para ser utilizado dentro de cada componente na listagem
+        keyExtractor={(item) => item}
+        //renderiza o componente
+        renderItem={({ item }) => <GroupCard title={item} onPress={() => {}} />}
+      />
     </Container>
   );
 }
