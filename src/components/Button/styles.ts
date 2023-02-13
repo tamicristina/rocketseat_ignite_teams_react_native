1f5fce32848abe7eb650546962525d18876ea4cc
Tamires <79336444+tamicristina@users.noreply.github.com>
2023-02-13 09:50:33 -0300

    create button types

---
+++ src/components/Button/styles.ts
@@ -0,0 +1,16 @@
+import { TouchableOpacity } from "react-native";
+import styled from "styled-components/native";
+
+export type ButtonTypeStyleProps = "PRIMARY" | "SECONDARY";
+
+type Props = {
+  type: ButtonTypeStyleProps;
+};
+
+export const Container = styled(TouchableOpacity)<Props>`
+  flex: 1;
+  min-height: 56px;
+  max-height: 56px;
+  background-color: ${({ theme, type }) =>
+    type === "PRIMARY" ? theme.COLORS.GRAY_700 : theme.COLORS.RED_DARK};
+`;
