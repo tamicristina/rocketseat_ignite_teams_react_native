03555b49b4773374b61b664be5bd950fc6ef44b4
Tamires <79336444+tamicristina@users.noreply.github.com>
2023-02-20 20:33:56 -0300

    add color to placeholder

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,6 +1,8 @@
-import { TextInputAndroidProps } from "react-native";
+import { TextInputProps } from "react-native";
 import { Container } from "./styles";
+import { useTheme } from "styled-components/native";
 
-export function Input({ ...rest }: TextInputAndroidProps) {
-  return <Container {...rest} />;
+export function Input({ ...rest }: TextInputProps) {
+  const { COLORS } = useTheme();
+  return <Container placeholderTextColor={COLORS.GRAY_300} {...rest} />;
 }
