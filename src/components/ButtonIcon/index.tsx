30e04470780685c18c3dff6db25172d570b725eb
Tamires <79336444+tamicristina@users.noreply.github.com>
2023-02-21 13:18:03 -0300

    create button icon component

---
+++ src/components/ButtonIcon/index.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+import { TouchableOpacityProps } from "react-native";
+
+import { Container, Icon } from "./styles";
+
+type Props = TouchableOpacityProps & {};
+
+export function ButtonIcon({}: Props) {
+  return (
+    <Container>
+      <Icon name="home" type="SECONDARY" />
+    </Container>
+  );
+}
