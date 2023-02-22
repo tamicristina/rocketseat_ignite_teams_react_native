5908831600e5347f96942787ed216ae4e5dcd801
Tamires <79336444+tamicristina@users.noreply.github.com>
2023-02-22 17:43:06 -0300

    update button icon

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,7 +1,8 @@
 import { Header } from "@components/Header";
 import { ButtonIcon } from "@components/ButtonIcon";
 import { Highlight } from "@components/Highlight";
-import { Container } from "./styles";
+import { Container, Form } from "./styles";
+import { Input } from "@components/Input";
 
 export function Players() {
   return (
@@ -11,7 +12,10 @@ export function Players() {
         title="Nome da turma"
         subtitle="Adicione a galera e separe os times"
       />
-      <ButtonIcon />
+      <Form>
+        <Input placeholder="Nome da pessoa" autoCorrect={false} />
+        <ButtonIcon icon="add" />
+      </Form>
     </Container>
   );
 }
